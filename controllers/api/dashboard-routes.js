e999e7f3e10c94d9917bfee2649cc8ca33c309c6
lgenzone <lgenzone@icloud.com>
2023-03-01 21:38:25 -0800

    updated home-routes and dashboard-routes (GET)

---
+++ controllers/api/dashboard-routes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const auth = require('../../utils/auth');
+
+const sequelize = require('../../config/connection');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content'
+            ],
+            include: [
+                {
+                    model: Comment, 
+                    attributes: [
+                        'id',
+                        'text',
+                        'user_id',
+                        'post_id'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const posts = dbPostData.map(post => post.get({ plain:true }));
+        res.render('dashboard', {posts, loggedIn: true });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('../api');
+const apiRoutes = require('.');
 const dashboardRoutes = require('./dashboard-routes');
 const homeRoutes = require('./home-routes');
 
